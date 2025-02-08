62371c9865f3c3203392a71080586a3f1e8047e5
Carlos Carabajal <carjuancara@gmail.com>
2025-02-08 12:01:49 -0300

    chore: :card_file_box: extended user model

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumber
 
-# Create your models here.
+class Users(AbstractUser):
+    address = models.CharField(max_length=200, blank=False, null=True)
+    phone = PhoneNumber(region='EU')
+
+    def __str__(self):
+        return self.address.capitalize()
+
+
