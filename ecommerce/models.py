d7f049aeaed58947aa3c85a84d2ec16a2605db96
Carlos Carabajal <carjuancara@gmail.com>
2025-02-08 12:18:50 -0300

    feat: :card_file_box: add Categories models

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -8,5 +8,13 @@ class Users(AbstractUser):
 
     def __str__(self):
         return self.address.capitalize()
+    
+class Categories(models.Model):
+    name = models.CharField(max_length=100, null=False, unique=True, blank=False)
+
+    def __str__(self):
+        return self.name.title()
+
+
 
 
