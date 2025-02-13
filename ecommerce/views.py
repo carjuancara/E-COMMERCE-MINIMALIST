402ec1710fcb154806824394d8d75bab064f8a07
Carlos Carabajal <carjuancara@gmail.com>
2025-02-13 10:26:43 -0300

    feat: :sparkles: add viewset
    viewset: ProductViewSet, ProfileViewSet, CategoryViewSet, OrderViewSet, OrderItemViewSet

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from ecommerce.serializers import ProductsSerializer, ProfileSerializer, CategoriesSerializer, OrdersSerializer, OrderItemSerializer
+from ecommerce.models import Products, Profile, Categories, Orders, OrderItem
 
-# Create your views here.
+class ProductViewSet(ModelViewSet):
+    queryset = Products.objects.all()
+    serializer_class = ProductsSerializer
+
+class ProfileViewSet(ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+class CategoryViewSet(ModelViewSet):
+    queryset = Categories.objects.all()
+    serializer_class = CategoriesSerializer
+
+class OrderViewSet(ModelViewSet):
+    queryset = Orders.objects.all()
+    serializer_class = OrdersSerializer
+
+class OrderItemViewSet(ModelViewSet):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
